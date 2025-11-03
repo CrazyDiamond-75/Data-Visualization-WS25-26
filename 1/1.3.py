4688da258a91a8463384c242653b80acf5ca20bc
Henri Heyden <stu240825@mail.uni-kiel.de>
2025-11-03 23:21:25 +0100

    Added 1.3.4

--- 1/1.3.py
+++ 1/1.3.py
@@ -2,25 +2,29 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib.colors import TABLEAU_COLORS
 import numpy as np #nunpy haha
-import math
+from sklearn.preprocessing import StandardScaler,MinMaxScaler #scikit-learn package
 
-# Store the dataset in a df, while specifying the header from the csv
-df = pd.read_csv('kochin.CSV', header=0)
+# Store the dataset in a df, while specifying the header, separator, and decimal period.
+df = pd.read_csv('kochin.CSV', header=0, sep=';', decimal=',')
+df = df.set_index('YEAR')
+df = pd.DataFrame(StandardScaler().fit_transform(df), columns=df.columns, index=df.index)
 
-fig, axes = plt.subplots()
-colors = list(TABLEAU_COLORS.keys())
-# For each subplot, we assign one name, dataset pair.
-for ax, (name, ds) in zip(axes, df.groupby('dataset')):
-    # Calculate the mean and variance x and y on the fly using numpy.
-    print(f"M=({np.mean(ds['x']):.4f}, {np.mean(ds['y']):.4f})\tV=({np.var(ds['x']):.4f}, {np.var(ds['y']):.4f})")
-    # Plot each ds as a scatterplot as needed.
-    ax.scatter(ds['x'], ds['y'], marker='.', color=colors[i % len(colors)])
-    ax.set_title(name)
-    i += 1
+cols = ['UNEMP', 'BWRATIO,', 'NNP'] # df.columns
 
-# Hide empty plots, as cols * rows <= n.
-for ax in axes[n:]:
-    ax.set_visible(False)
+fig, ax = plt.subplots(figsize=(16,9))
+years = list(df.index.values)
 
-plt.tight_layout()
-plt.savefig("1.2.pdf")
+for cname in cols:
+    col = df[cname]
+    ax.plot(col, label=cname)
+
+ax.set_xlabel('Years')
+ax.set_xticks(years)
+ax.set_ylabel('Normalized attributes')
+ax.set_yticks([])
+ax.set(xlim=(years[0], years[-1]))
+ax.set_title('Unemployment does not strongly correlate with net national product or benefit/wage ratio')
+ax.legend()
+
+plt.savefig('1.3.4.pdf')
+
