ee2a4ba65c7b77c932f0706f6ebbcf2e88da9eaf
Henri Heyden <stu240825@mail.uni-kiel.de>
2025-11-04 15:12:55 +0100

    More or less completed everything

--- 1/1.3.py
+++ 1/1.3.py
@@ -30,4 +30,27 @@ ax.set(xlim=(years[0], years[-1]))
 ax.set_title('Unemployment did not strongly correlate with net national product or benefit/wage ratio')
 ax.legend()
 
-plt.savefig('1.3.45.pdf')
+#plt.savefig('1.3.45.pdf')
+
+#### PLOT FOR 1.3.7 ####
+# Columns we want for the first plot.
+cols = ['UNEMP', 'DEMAND'] # df.columns
+
+# Generate one plot only, specify the figure size.
+fig, ax = plt.subplots(figsize=(16,9))
+years = list(df.index.values)
+
+for cname in cols:
+    col = df[cname]
+    ax.plot(col, label=cname)
+
+ax.set_xlabel('Years')
+ax.set_xticks(years)
+ax.set_ylabel('Normalized scale')
+ax.set_yticks([])
+ax.set(xlim=(years[0], years[-1]))
+ax.set_title('Unemployment correlated with demand')
+ax.legend()
+
+plt.savefig('1.3.7.pdf')
+
