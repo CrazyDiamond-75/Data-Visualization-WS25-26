01912c1a98f45040e832b67fb03a562fd528d874
Henri Heyden <stu240825@mail.uni-kiel.de>
2025-11-04 14:17:25 +0100

    Cleaned up code and refreshed requirements.txt

--- 1/1.3.py
+++ 1/1.3.py
@@ -1,16 +1,20 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib.colors import TABLEAU_COLORS
-import numpy as np #nunpy haha
-from sklearn.preprocessing import StandardScaler,MinMaxScaler #scikit-learn package
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
 
 # Store the dataset in a df, while specifying the header, separator, and decimal period.
 df = pd.read_csv('kochin.CSV', header=0, sep=';', decimal=',')
+# Set the index to the year for easier plotting later.
 df = df.set_index('YEAR')
+# Normalize all attribute values by subtracting their mean and dividing their standard deviation.
 df = pd.DataFrame(StandardScaler().fit_transform(df), columns=df.columns, index=df.index)
 
+#### PLOT FOR 1.3.4 and 1.3.5 ####
+# Columns we want for the first plot.
 cols = ['UNEMP', 'BWRATIO,', 'NNP'] # df.columns
 
+# Generate one plot only, specify the figure size.
 fig, ax = plt.subplots(figsize=(16,9))
 years = list(df.index.values)
 
@@ -20,11 +24,10 @@ for cname in cols:
 
 ax.set_xlabel('Years')
 ax.set_xticks(years)
-ax.set_ylabel('Normalized attributes')
+ax.set_ylabel('Normalized scale')
 ax.set_yticks([])
 ax.set(xlim=(years[0], years[-1]))
-ax.set_title('Unemployment does not strongly correlate with net national product or benefit/wage ratio')
+ax.set_title('Unemployment did not strongly correlate with net national product or benefit/wage ratio')
 ax.legend()
 
-plt.savefig('1.3.4.pdf')
-
+plt.savefig('1.3.45.pdf')
